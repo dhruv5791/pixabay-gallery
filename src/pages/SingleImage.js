d0825cee08ef54d0a90b360373169d64a619cc74
dhruv <dddeadz@gmail.com>
2021-04-16 12:17:27 -0400

    Fixed issue with single image route

--- src/pages/SingleImage.js
+++ src/pages/SingleImage.js
@@ -5,22 +5,21 @@ const API_KEY = "21145804-b1dabd64212070a566be72dd1";
 
 const SingleImage = () => {
   let { id } = useParams();
-  const [imageId, setImageId] = useState(id);
-  const [image, setImage] = useState([]);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
-    if (typeof imageId === "number") {
+    if (!isNaN(parseInt(id))) {
       fetch(
-        `https://pixabay.com/api/?key=${API_KEY}&id=${imageId}&image_type=photo&&pretty=true`
+        `https://pixabay.com/api/?key=${API_KEY}&id=${id}&image_type=photo&&pretty=true`
       )
         .then((response) => response.json())
         .then((data) => setImage(data.hits[0]));
     }
-  }, [imageId]);
+  }, []);
   return (
     <div className="flex justify-center h-screen items-center">
       <div className="max-w-lg">
-        {image && image.length ? (
+        {image ? (
           <ImageCard image={image} />
         ) : (
           <h1>No image found with given ID.</h1>
